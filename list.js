8da4f08a22684ae241d99ac8d6fb8b47b66d53e7
Susana Rodriguez <susana.rodriguez@bbc.co.uk>
2020-10-31 12:44:42 +0000

    NO_TICKET - Create list endpoint

---
+++ list.js
@@ -0,0 +1,16 @@
+import handler from "./libs/handler";
+import dynamoDb from "./libs/dynamodb";
+
+export const main = handler(async (event, context) => {
+  const params = {
+    TableName: process.env.tableName,
+    KeyConditionExpression: "userId = :userId",
+    ExpressionAttributeValues: {
+      ":userId": event.requestContext.identity.cognitoIdentityId,
+    },
+  };
+
+  const result = await dynamoDb.query(params);
+
+  return result.Items;
+});
